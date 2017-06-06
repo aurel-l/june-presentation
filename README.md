4a3e8fc70b26678c056db0125589395fb0d586c2
Aurélien Luciani <luciani@ebi.ac.uk>
2017-06-06 11:51:40 +0100

    add README

---
+++ README.md
@@ -0,0 +1,9 @@
+### Presentation for group meeting June 2017
+
+**Only intended for internal use**
+
+Not supported in any way. It was just to try things with WebComponents. Supposed to work at least on Chrome 60+.
+
+ - `npm install` to install dependencies
+ - `npm run start` to start a dev server with live-reload
+ - `npm run build` to create a build
--- src/index.js
+++ src/index.js
@@ -6,31 +6,31 @@ import defineWebComponent from 'utils/defineWebComponent';
   await defineWebComponent(() =>
     import(
       /* webpackChunkName: 'presentation-footer' */
-      'web-components/presentation-footer',
-    ),
+      'web-components/presentation-footer'
+    )
   );
   await defineWebComponent(
     () =>
       import(
         /* webpackChunkName: 'presentation-slides' */
-        'web-components/presentation-slides',
+        'web-components/presentation-slides'
       ),
     null,
     {
       slideSelector: 'section, presentation-slide',
-    },
+    }
   );
   await defineWebComponent(() =>
     import(
       /* webpackChunkName: 'presentation-event' */
-      'web-components/presentation-event',
-    ),
+      'web-components/presentation-event'
+    )
   );
   await defineWebComponent(() =>
     import(
       /* webpackChunkName: 'presentation-manager' */
-      'web-components/presentation-manager',
-    ),
+      'web-components/presentation-manager'
+    )
   );
   await defineWebComponent(
     () => import(/* webpackChunkName: 'data-loader' */ 'data-loader'),
